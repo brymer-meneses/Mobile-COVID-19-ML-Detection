0aaa4c8c71a3e05e714b2007729f3d919f71fd02
Brymer Meneses <68867487+Brymer-Meneses@users.noreply.github.com>
2021-02-28 18:20:39 +0800

    Update classifier_float.dart

--- Flutter Source/lib/ml-services/classifier_float.dart
+++ Flutter Source/lib/ml-services/classifier_float.dart
@@ -1,3 +1,21 @@
+//    Copyright 2020, Amish Garg
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+//
+//    Modified from: 
+//    https://github.com/am15h/tflite_flutter_helper/blob/master/example/image_classification/lib/classifier_float.dart
+
+
 import 'package:tflite_flutter_helper/tflite_flutter_helper.dart';
 
 import './classifier.dart';
